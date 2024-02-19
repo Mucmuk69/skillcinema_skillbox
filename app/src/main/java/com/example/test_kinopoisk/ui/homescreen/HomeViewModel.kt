0eb64645d4dd5c9f716df9f6e69f2d0cee731665
Mucmuk69 <skeggi69@gmail.com>
2024-02-19 09:55:04 +0300

    test commit after restore

--- app/src/main/java/com/example/test_kinopoisk/ui/homescreen/HomeViewModel.kt
+++ app/src/main/java/com/example/test_kinopoisk/ui/homescreen/HomeViewModel.kt
@@ -3,6 +3,4 @@ package com.example.test_kinopoisk.ui.homescreen
 import androidx.lifecycle.ViewModel
 
 
-class HomeViewModel private constructor(
-
-) : ViewModel()
+class HomeViewModel private constructor() : ViewModel()
